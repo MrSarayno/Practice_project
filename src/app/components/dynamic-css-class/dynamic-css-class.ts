73832bca705fb9282711215c6fb9391b67a8c617
Niño Francis Sarayno <mrsarayno@gmail.com>
2026-01-11 03:47:30 +0800

    done Dynamic Css CLass

--- src/app/components/dynamic-css-class/dynamic-css-class.ts
+++ src/app/components/dynamic-css-class/dynamic-css-class.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-css-class',
-  imports: [],
+  imports: [NgClass, FormsModule, NgStyle],
   templateUrl: './dynamic-css-class.html',
   styleUrl: './dynamic-css-class.css',
 })
 export class DynamicCssClass {
+  myClassName: string = "bg-warning text-center";
 
+  isActive: boolean = false;
+
+  ProductPrice = 500;
+
+  divBackColor: string = '';
+
+  isSidePanel = signal(false); 
+
+  openSidePanel() {
+    this.isSidePanel.set(true);
+  }
+
+  closeSidePanel() {
+    this.isSidePanel.set(false);
+  }
+
+  myCss = {
+    color: 'red',
+    'background-color': 'black',
+    'font-size': '35px'
+  }
 }
