0ab1c5e4e58c13be3711b5cc693fa387e15457d2
Niño Francis Sarayno <mrsarayno@gmail.com>
2026-01-11 21:04:56 +0800

    new

--- src/app/components/API CALL/get-api/get-api.ts
+++ src/app/components/API CALL/get-api/get-api.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class GetAPI {
 
+  userList: any[] = [];
+  productList: any[] = [];
+  
+  constructor(private http: HttpClient) {
+  
+  }
+
+  getUsers() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((result:any)   => {
+      this.userList = result;
+    })
+  }
+
+  getProducts() {
+    this.http.get('https://fake-store-api.mock.beeceptor.com/api/products').subscribe((result:any)   => {
+      this.productList = result;
+    })
+  }
 }
