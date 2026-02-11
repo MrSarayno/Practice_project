0b5ed4e4bc266fadbbc709defb42558adec94f18
Niño Francis Sarayno <mrsarayno@gmail.com>
2026-02-11 12:36:18 +0800

    new

--- src/app/components/API CALL/post-api/post-api.ts
+++ src/app/components/API CALL/post-api/post-api.ts
@@ -12,13 +12,13 @@ export class PostAPI {
   carList: any[] = [];
   carObj: any = {
     CarId: 0,
-    Brand: " ",
-    Model: " ",
-    Year: " ",
-    Color: " ",
-    DailyRate: " ",
-    CarImage: "",
-    RegNo: " ",
+    Brand: ' ',
+    Model: ' ',
+    Year: ' ',
+    Color: ' ',
+    DailyRate: ' ',
+    CarImage: '',
+    RegNo: ' ',
   };
   http = inject(HttpClient);
 
@@ -27,18 +27,53 @@ export class PostAPI {
       .get('https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars')
       .subscribe((res: any) => {
         this.carList = res.data;
-      });
+      })
   }
 
   onSaveCar() {
     debugger;
-    this.http.post('https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewCar',this.carObj).subscribe((res:any)=>{
-      debugger;
-      if(res.result) {
-        alert("Car Created Successfully")
-      } else {
-        alert(res.message)
-      }
-    })
+    this.http
+      .post('https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewCar', this.carObj)
+      .subscribe((res: any) => {
+        debugger;
+        if (res.result) {
+          alert('Car Created Successfully');
+        } else {
+          alert(res.message);
+        }
+      })
+  }
+
+  onEdit(data: any) {
+    this.carObj = data;
+  }
+
+  updateCar() {
+    this.http
+      .put('https://freeapi.miniprojectideas.com/api/CarRentalApp/UpdateCar', this.carObj)
+      .subscribe((res: any) => {
+        if (res.result) {
+          alert('Car Updated Successfully');
+        } else {
+          alert(res.message);
+        }
+      })
+  }
+
+  onDelete(id: number) {
+    const isDelete = confirm('Are you sure to delete this?');
+    if (isDelete == true) {
+      this.http
+        .delete(
+          'https://freeapi.miniprojectideas.com/api/CarRentalApp/DeleteCarbyCarId?cardid=' + id,
+        )
+        .subscribe((res: any) => {
+          if (res.result) {
+            alert('Car Deleted Successfully');
+          } else {
+            alert(res.message);
+          }
+        })
+    }
   }
 }
